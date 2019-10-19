8be7c1c2e3d932a5ac3d9fa90da9d2a3b783d0a6
unknown <germandpascual@hotmail.com>
2019-10-19 01:47:25 -0300

    new file:   __init__.py 	new file:   bullet5.png 	new file:   bulletbig.png 	new file:   bulletbig2.png 	new file:   bulletbig2_tr.png 	new file:   clases.py 	new file:   clases.pyc 	new file:   ejemplo instruccion.asm 	new file:   eventos.txt 	new file:   msgothic.ttc 	modified:   placeenemy_tr.png 	new file:   player/2608_bd.wav 	new file:   player/2608_hh.wav 	new file:   player/2608_rim.wav 	new file:   player/2608_rym.wav 	new file:   player/2608_sd.wav 	new file:   player/2608_tom.wav 	new file:   player/2608_top.wav 	new file:   player/FMPMDE.exe 	new file:   player/FMPMDE.ini 	new file:   player/FMPMDE.txt 	new file:   player/GASSAKU.M2 	new file:   player/GS_PCM.PPC 	new file:   player/PMDWin.dll 	new file:   player/PlayList.m3u 	new file:   player/Th05_06.M 	new file:   player/WinFMP.dll 	new file:   player/leeme gilastrun.txt 	new file:   player/prueba1.M2 	new file:   player/prueba1.mml 	new file:   player/test.M2 	modified:   prueba1.py

---
+++ Piton/clases.py
@@ -0,0 +1,138 @@
+import pygame, sys
+from pygame.locals import *
+pygame.init()
+
+class Enemy(pygame.sprite.Sprite):
+	def __init__(self,startpos,pattern,patternspeed,delay):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load("placeenemy_tr.png")
+		self.rect = self.image.get_rect()
+		self.rect.inflate_ip(-6,-6)
+		self.init(startpos)
+		htbxlist.append(self.rect)
+		
+	def init(self,startpos):
+		self.rect.x = startpos[0]
+		self.rect.y = startpos[1]
+		self.rect.move([0,0])
+		
+	def update(self):
+		
+		if self.rect.collidepoint(600,60):
+			self.movepos = [-2,0]
+		elif self.rect.collidepoint(20,60):
+			self.movepos = [2,0]
+			
+		self.rect.move_ip(self.movepos)
+		pygame.event.pump()
+		
+	def delete(self):
+		enemies.remove(self)
+		htbxlist.remove(self.rect)
+		
+class Bullet(pygame.sprite.Sprite):
+	def __init__(self,speed,startpos):
+		pygame.sprite.Sprite.__init__(self)
+		self.init(speed,startpos)
+		htbxlist.append(self.rect)
+		global debugbulletcount
+		debugbulletcount += 1
+	
+	def init(self,speed,startpos):
+		self.rect.x = startpos[0]
+		self.rect.y = startpos[1]
+		self.movepos = [speed[0],speed[1]]
+		
+	def update(self):
+		self.rect.move_ip(self.movepos)
+		pygame.event.pump()
+		if not area.contains(self.rect):
+			self.delete()
+		
+	def delete(self):
+		bullets.remove(self)
+		htbxlist.remove(self.rect)
+		
+	def __del__(self):
+		global debugbulletcount
+		debugbulletcount -= 1
+		
+class BulletA(Bullet):
+	def __init__(self,speed,startpos):
+		self.image = pygame.image.load("bullet3_tr.png")
+		self.rect = self.image.get_rect()
+		self.rect.inflate_ip(-6,-6)
+		Bullet.__init__(self,speed,startpos)
+
+class BulletB(Bullet):
+	def __init__(self,speed,startpos):
+		self.image = pygame.image.load("bulletbig2_tr.png")
+		self.rect = self.image.get_rect()
+		self.rect.inflate_ip(-25,-25)
+		Bullet.__init__(self,speed,startpos)
+		
+class Player(pygame.sprite.Sprite):
+	
+	def __init__(self):
+		pygame.sprite.Sprite.__init__(self)
+		self.image = pygame.image.load("placehtbx2_tr.png")
+		self.rect = self.image.get_rect()
+		self.rect.inflate_ip(-10,-10)
+		self.state = [0,0,6]
+		self.invulntime = 0
+		self.init()
+		
+	def init(self):
+		self.rect.x = 300
+		self.rect.y = 300
+		self.state = [0,0,6]
+		self.movepos = [0,0]
+
+	def update(self):
+		self.movepos = [(self.state[0] * self.state[2]),(self.state[1] * self.state[2])]
+		newpos = self.rect.move(self.movepos)
+		if area.contains(newpos):
+			self.rect = newpos
+			pygame.event.pump()
+		if not self.invulntime == 0:
+			self.invulntime -= 1
+			
+	def hit(self):
+			self.rect.x = 300
+			self.rect.y = 300
+			self.invulntime = 150
+
+	def moveup(self):
+		if self.state[1] == 1:
+			self.state[1] = 0
+		else:
+			self.state[1] = -1
+			
+	def movedown(self):
+		if self.state[1] == -1:
+			self.state[1] = 0
+		else:
+			self.state[1] = 1
+	
+	def moveleft(self):
+		if self.state[0] == 1:
+			self.state[0] = 0
+		else:
+			self.state[0] = -1
+			
+	def moveright(self):
+		if self.state[0] == -1:
+			self.state[0] = 0
+		else:
+			self.state[0] = 1
+
+size = width, height = 640, 480
+screen = pygame.display.set_mode(size)
+player = Player()
+area = screen.get_rect()
+clock = pygame.time.Clock()
+clock
+font = pygame.font.Font("MSGOTHIC.TTC", 20)
+bullets = list()
+htbxlist = list()
+enemies = list()
--- Piton/prueba1.py
+++ Piton/prueba1.py
@@ -1,182 +1,72 @@
-import pygame, sys
-from pygame.locals import *
-pygame.init()
+import clases
+from clases import *
 
-bulletcount = [0]
+debugbulletcount = 0
 
-class Enemy(pygame.sprite.Sprite):
-	def __init__(self,startpos,speed):
-		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.image.load("placeenemy_tr.png")
-		self.rect = self.image.get_rect()
-		self.rect.inflate_ip(-6,-6)
-		self.init(startpos)
-		self.movepos = speed
-		htbxlist.append(self.rect)
-		self.bulletcounter = 0
-		
-	def init(self,startpos):
-		self.rect.x = startpos[0]
-		self.rect.y = startpos[1]
-		self.rect.move([0,0])
-		
-	def update(self):
-		
-		if self.rect.collidepoint(600,60):
-			self.movepos = [-2,0]
-		elif self.rect.collidepoint(20,60):
-			self.movepos = [2,0]
-			
-		self.rect.move_ip(self.movepos)
-		pygame.event.pump()
-		if not area.contains(self.rect):
-			self.delete()
-		self.bulletcounter += 1
-		if self.bulletcounter == 20:
-			bullet.append(Bullet([0,5],[self.rect.x,self.rect.y + 40]))
-			bullet.append(Bullet([-2,4],[self.rect.x - 50,self.rect.y + 40]))
-			bullet.append(Bullet([2,4],[self.rect.x + 50,self.rect.y + 40]))
-			self.bulletcounter = 0
-		
-	def delete(self):
-		enemy.remove(self)
-		htbxlist.remove(self.rect)
-		
-class Bullet(pygame.sprite.Sprite):
-	def __init__(self,speed,startpos):
-		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.image.load("bullet3_tr.png")
-		self.rect = self.image.get_rect()
-		self.rect.inflate_ip(-8,-8)
-		self.init(speed,startpos)
-		htbxlist.append(self.rect)
-		bulletcount[0] += 1
-	
-	def init(self,speed,startpos):
-		self.rect.x = startpos[0]
-		self.rect.y = startpos[1]
-		self.movepos = [speed[0],speed[1]]
-		
-	def update(self):
-		self.rect.move_ip(self.movepos)
-		pygame.event.pump()
-		if not area.contains(self.rect):
-			self.delete()
-		
-	def delete(self):
-		bullet.remove(self)
-		htbxlist.remove(self.rect)
-		
-	def __del__(self):
-		bulletcount[0] -= 1
-		
-class Player(pygame.sprite.Sprite):
-	
-	def __init__(self):
-		pygame.sprite.Sprite.__init__(self)
-		self.image = pygame.image.load("placehtbx2_tr.png")
-		self.rect = self.image.get_rect()
-		self.rect.inflate_ip(-10,-10)
-		self.state = [0,0,6]
-		self.init()
-		
-	def init(self):
-		self.rect.x = 300
-		self.rect.y = 300
-		self.state = [0,0,6]
-		self.movepos = [0,0]
+eventdict = {}
+eventfile = open("eventos.txt","r")
+eventlines = eventfile.readlines()
+eventlist = list()
+for i in eventlines:
+    eventlist.append((i.split()))
 
-	def update(self):
-		self.movepos = [(self.state[0] * self.state[2]),(self.state[1] * self.state[2])]
-		newpos = self.rect.move(self.movepos)
-		if area.contains(newpos):
-			self.rect = newpos
-			pygame.event.pump()
-		
-	def moveup(self):
-		if self.state[1] == 1:
-			self.state[1] = 0
-		else:
-			self.state[1] = -1
-			
-	def movedown(self):
-		if self.state[1] == -1:
-			self.state[1] = 0
-		else:
-			self.state[1] = 1
-	
-	def moveleft(self):
-		if self.state[0] == 1:
-			self.state[0] = 0
-		else:
-			self.state[0] = -1
-			
-	def moveright(self):
-		if self.state[0] == -1:
-			self.state[0] = 0
-		else:
-			self.state[0] = 1
-		
-size = width, height = 640, 480
-screen = pygame.display.set_mode(size)
-player = Player()
-area = screen.get_rect()
-clock = pygame.time.Clock()
-clock
-font = pygame.font.Font("C:\Windows\Fonts\MSGOTHIC.TTC", 20)
-bullet = list()
-htbxlist = list()
-enemy = list()
-enemy.append(Enemy([300,60],[4,0]))
-enemy.append(Enemy([300,60],[-4,0]))
-while 1:
-	clock.tick_busy_loop(60)
-	for event in pygame.event.get():
-		if event.type == pygame.QUIT: sys.exit()
-		
-		elif event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_LSHIFT:
-				player.state[2] = 2
-				player.update()
-			if event.key == pygame.K_UP:
-					player.moveup()
-			elif event.key == pygame.K_DOWN:
-					player.movedown()
-			elif event.key == pygame.K_LEFT:
-					player.moveleft()
-			elif event.key == pygame.K_RIGHT:
-					player.moveright()
-					
-		elif event.type == pygame.KEYUP:
-			if event.key == pygame.K_LSHIFT:
-				player.state[2] = 6
-				player.update()
-			if event.key == pygame.K_UP:
-					player.movedown()
-			elif event.key == pygame.K_DOWN:
-					player.moveup()
-			elif event.key == pygame.K_LEFT:
-					player.moveright()
-			elif event.key == pygame.K_RIGHT:
-					player.moveleft()
-					
-	screen.fill(0)
-	
-	player.update()
-	for i in enemy:
-		i.update()
-		screen.blit(i.image, i.rect)
-	for i in bullet:
-		i.update()
-		screen.blit(i.image, i.rect)
-		
-	screen.blit(player.image, player.rect)
-	
-	fps = font.render(str(int(clock.get_fps()) - 2) + "FPS", True, (255,255,255))
-	bulletcountdisp = font.render("Balas:" + str(bulletcount[0]), True, (255,255,255))
-	screen.blit(fps, area)
-	screen.blit(bulletcountdisp, (screen.get_width() - bulletcountdisp.get_width(),screen.get_height() - bulletcountdisp.get_height()))
-	if player.rect.collidelist(htbxlist) != -1:
-		player.rect.x = 300
-		player.rect.y = 300
-	pygame.display.flip()
+def seekplayer(enemy):
+	angle = 0
+	return
+
+def runtime():
+    while 1:
+        clock.tick_busy_loop(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT: sys.exit()
+            
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LSHIFT:
+                    player.state[2] = 2
+                    player.update()
+                if event.key == pygame.K_UP:
+                        player.moveup()
+                elif event.key == pygame.K_DOWN:
+                        player.movedown()
+                elif event.key == pygame.K_LEFT:
+                        player.moveleft()
+                elif event.key == pygame.K_RIGHT:
+                        player.moveright()
+                        
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_LSHIFT:
+                    player.state[2] = 6
+                    player.update()
+                if event.key == pygame.K_UP:
+                        player.movedown()
+                elif event.key == pygame.K_DOWN:
+                        player.moveup()
+                elif event.key == pygame.K_LEFT:
+                        player.moveright()
+                elif event.key == pygame.K_RIGHT:
+                        player.moveleft()
+                        
+        screen.fill(0)
+        
+        player.update()
+        for i in enemies:
+            i.update()
+            screen.blit(i.image, i.rect)
+        for i in bullets:
+            i.update()
+            screen.blit(i.image, i.rect)
+            
+        screen.blit(player.image, player.rect)
+        
+        fps = font.render(str(int(clock.get_fps()) - 2) + "FPS", True, (255,255,255))
+        bulletcountdisp = font.render("Balas:" + str(debugbulletcount), True, (255,255,255))
+        screen.blit(fps, area)
+        screen.blit(bulletcountdisp, (screen.get_width() - bulletcountdisp.get_width(),screen.get_height() - bulletcountdisp.get_height()))
+        if player.invulntime == 0:
+            if player.rect.collidelist(htbxlist) != -1:
+                player.hit()
+        elif player.invulntime > 90:
+            for i in bullets:
+                i.delete()
+        pygame.display.flip()
+runtime()
